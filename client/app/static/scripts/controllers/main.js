b25b6cc3747f551cd26e9a58c1310bb02349bb3d
AnderSilva <anderson.goma@gmail.com>
2017-11-02 22:30:53 -0200

    updates

--- client/app/static/scripts/controllers/main.js
+++ client/app/static/scripts/controllers/main.js
@@ -10,8 +10,10 @@
 angular.module('clientApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [
-      'Create Django models',
-      'Expose them through a REST API',
-      'Consume them with restangular'
+      'Criar as models no Django, baseado nas tabelas',
+      'Definir quais servicos o front vai precisar',
+      'Implementar os serviços na API para o client(front) consumir',
+      'Ajeitar tudo isso aqui pra ficar mais bonito',
+      'O que o time achar que precisa ser feito'
     ];
   });
